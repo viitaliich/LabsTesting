3aa03dcf6b8d05a50790b1bb2fa0af869af01ffa
Vitalii Klimov <vitklim11@gmail.com>
2021-04-13 17:16:51 +0300

    run_program function

--- LabsTesting/src/main.cpp
+++ LabsTesting/src/main.cpp
@@ -1,6 +1,77 @@
+// TODO: 
+//	path mechanism
+//	format
+
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <memory>
+#include <fstream>
+
+std::string run_program(std::string& path);
+
+std::string run_program(std::string& path) {
+	
+	// TODO: it can be replaced with identification by thead number (thread ID)
+
+	size_t index = path.length()-1;
+	while (path[index] != '.' && index > 0) {
+		index--;
+	}
+	size_t name_end = index - 1;
+
+	while (path[index] != '\\' && index > 0) {
+		index--;
+	}
+	size_t name_start = index + 1;
+
+	size_t length = 0;
+	if (name_end != 0 && name_start != 0 && name_end != name_start)
+		length = name_end - name_start + 1;
+	else {
+		std::cout << "ERROR: file path is wrong\n";
+		exit(1);
+	}
+	std::string file_name = path.substr(name_start, length);	// +".txt";
+	
+	std::stringstream ss;
+	ss << path << " > " << file_name;
+	std::string cmd(ss.str());
+	ss << system(cmd.c_str());
+
+	std::ifstream res_file;
+	res_file.open(file_name);
+	if (res_file.is_open()) {
+		std::string res;
+		getline(res_file, res);
+		res_file.close();
+		return res;
+	}
+	else {
+		std::cout << "ERROR: file [" << file_name << "] is not opened\n";
+		exit(1);
+	}
+}
+
+void modify_in_code();
+
+void modify_input_code(std::string& data, const std::string& path) {
+	std::ofstream file(path);
+	file << data;
+	file.close();
+}
+
 
 int main(int argc, char** argv) {
-	std::cout << "Hello World" << std::endl;
+	
+	std::string path1 = "D:\\KPI\\LabsTesting\\LabsTesting\\CompilerLabsTest.exe";
+	std::string res1 = run_program(path1);
+
+	std::string path2 = ".\\source.exe";
+	std::string res2 = run_program(path2);
+
+	std::cout << res2;
+	
+	//getchar();
 	return 0;
 }
