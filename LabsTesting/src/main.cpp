e5856cccee10c6885316dcd6ad0453474545e36f
Vitalii Klimov <vitklim11@gmail.com>
2021-05-16 11:12:10 +0300

    GLFW, GLEW, ImGUI basics, added boost

--- LabsTesting/src/main.cpp
+++ LabsTesting/src/main.cpp
@@ -4,6 +4,12 @@
 #include <memory>
 #include <fstream>
 
+#include <GL/glew.h>		// maybe we don't need this include here, because we don't draw anything ???
+#include <GLFW/glfw3.h>		// must be included after <GL/glew.h>
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
 #include "common.h"
 #include "TestGen.h"
 
@@ -68,10 +74,95 @@ void modify_input_code(std::string& data, const std::string& path) {
 
 
 int main(int argc, char** argv) {
-	
+
+
+	// Probably make GLFW thing as a separate function
+	// GLFW below
+
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	// Must be after setting of valid context
+	// maybe we don't need this here, because we don't use OpenGL functions ???
+	if (glewInit() != GLEW_OK)
+		std::cout << "ERROR: not GLEW_OK\n";
+
+	ImGui::CreateContext();
+	ImGui_ImplGlfwGL3_Init(window, true);
+
+	// Setup style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// Variables for ImGui num.1 example
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		ImGui_ImplGlfwGL3_NewFrame();	
+
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);		// maybe rid of this
+
+		// 1. Show a simple window.
+		{
+			static float f = 0.0f;
+			static int counter = 0;
+			ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
+			ImGui::Checkbox("Another Window", &show_another_window);
+
+			if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+				counter++;
+			ImGui::SameLine();
+			ImGui::Text("counter = %d", counter);
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		}
+
+		ImGui::Render();
+		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	// Cleanup
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwTerminate();
+
+
+	// *******************************
 	TestGen* tg = new TestGen(1);
 	tg->Generate();
 	std::cout << "DONE";
+	// *******************************
 
 	//getchar();
 	return 0;
